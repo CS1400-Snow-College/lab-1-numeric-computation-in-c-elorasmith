04cfac4e24524cd01d15efd3ae955e35cc494571
elorasmith <eloraasmith@gmail.com>
2025-01-22 16:07:45 -0700

    Step 1

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int a = 18;
+int b = 6;
+int c = a + b;
+Console.WriteLine(c);
+int d = a - b;
+Console.WriteLine(d);
+int e = a * b;
+Console.WriteLine(e);
+int f = a/b;
+Console.WriteLine(f);
+
