e8d9004e5fbaa0131e3f5eb3e8a2e0cca054c9cb
elorasmith <eloraasmith@gmail.com>
2025-01-22 16:29:11 -0700

    Step 2

--- Program.cs
+++ Program.cs
@@ -12,3 +12,16 @@ Console.WriteLine(e);
 int f = a/b;
 Console.WriteLine(f);
 
+int g = 5;
+int h = 4;
+int i = 2;
+int j = (g + h) * i;
+Console.WriteLine(j);
+int k = 6 + h *(a/i)-12 * g;
+Console.WriteLine(k);
+int l = 7;
+int m = 4;
+int n = 3;
+int o = (l + m)/n;
+Console.WriteLine(o);
+
